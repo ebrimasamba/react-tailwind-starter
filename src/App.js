36db69e5260a6678794edfb7a879dd9b67852c6e
Ebrima Samba <ebrima.samba.75@gmail.com>
2021-02-01 16:44:58 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-500 text-white text-2xl p-5 text-center">
+        React Tailwind starter
+      </h1>
+    </div>
+  )
+}
+
+export default App
